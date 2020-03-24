bebefddc52426ddbcd92b8c5f470ed3b12ed2e50
ryik5 <ryik.yuri@gmail.com>
2020-03-24 21:37:55 +0200

    fixed some errrors

--- Form1.cs
+++ Form1.cs
@@ -98,7 +98,7 @@ namespace AutoAnalyse
         public string F { get; set; }
         public string I { get; set; }
         public string O { get; set; }
-        public DateTime Birthday { get; set; }
+        public string Birthday { get; set; }
         public string City { get; set; }
         public string District { get; set; }
         public string Street { get; set; }
@@ -106,7 +106,7 @@ namespace AutoAnalyse
         public string BuildingBody { get; set; }
         public string Apartment { get; set; }
         public string CodeOperation { get; set; }
-        public DateTime CodeDate { get; set; }
+        public string CodeDate { get; set; }
 
 
         public override string ToString()
@@ -200,66 +200,181 @@ namespace AutoAnalyse
     public static class GetModel<T> where T : IRow
     {
 
-        public static T ToModel(string row)
+        public static T ToModel(string source)
         {
             string s = typeof(T).ToString();
-            IRow car;
+            IRow row;
             switch (s)
             {
                 case "Car":
-                    car = new Car();
-                    RowToCar model = new RowToCar(row);
-                    car = model.Get();
-                    return (T)car;
+                    ParserRowToCar model = new ParserRowToCar(source);
+                    row = model.Get();
+                    return (T)row;
 
+                case "Owner":
                 default:
-                    car = null;
-                    return (T)car;
+                    ParserRowToOwner model1 = new ParserRowToOwner(source);
+                    row = model1.Get();
+                    return (T)row;
             }
         }
     }
 
-    public class RowToCar
+    public class ParserRowToOwner
     {
-        private Car car;
+        private IRow owner;
         private string _row;
-        IList<string> _orderNameColumns;
-        IList<int> _orderNumberColumns = new List<int>();
+       
         public int[] numberColumns = new int[1];
         public string[] parsedColumns = new string[1];
         public string[] nameColumns = new string[1];
 
 
-        public RowToCar(string rowData)
+        public ParserRowToOwner(string rowData)
         { _row = rowData; }
 
-        public void ConvertRowToCar()
+        public void ConvertRowToModel()
         {
             parsedColumns = _row.Split('|');
-            car = new Car();
-            car.Plate = parsedColumns[numberColumns[0]];
-            car.Factory = parsedColumns[numberColumns[1]];
-            car.Model = parsedColumns[numberColumns[2]];
-            car.ManufactureYear = parsedColumns[numberColumns[3]];
-            car.BodyNumber = parsedColumns[numberColumns[4]];
-            car.ChassisNumber = parsedColumns[numberColumns[5]];
-            car.EngineVolume = parsedColumns[numberColumns[6]];
+            owner = new Owner
+            {
+                Name = parsedColumns[numberColumns[0]],
+                Type = parsedColumns[numberColumns[3]]?.Length > 0 ? TypeOwner.Enterprise : TypeOwner.Person,
+
+                Address = $"{parsedColumns[numberColumns[9]]}/t{parsedColumns[numberColumns[10]]}/t" +
+                $"{parsedColumns[numberColumns[11]]}/t{parsedColumns[numberColumns[12]]}/t" +
+                $"{parsedColumns[numberColumns[13]]}/t{parsedColumns[numberColumns[14]]}",
+
+                EDRPOU = int.TryParse(parsedColumns[numberColumns[3]], out int edrpou) ? edrpou : 0,
+                DRFO = int.TryParse(parsedColumns[numberColumns[4]], out int drfo) ? drfo : 0,
+                F = parsedColumns[numberColumns[5]],
+                I = parsedColumns[numberColumns[6]],
+                O = parsedColumns[numberColumns[7]],
+                Birthday = parsedColumns[numberColumns[8]],
+                City = parsedColumns[numberColumns[9]],
+                District = parsedColumns[numberColumns[10]],
+                Street = parsedColumns[numberColumns[11]],
+                Building = parsedColumns[numberColumns[12]],
+                BuildingBody = parsedColumns[numberColumns[13]],
+                Apartment = parsedColumns[numberColumns[14]],
+                CodeOperation = parsedColumns[numberColumns[15]],
+                CodeDate = parsedColumns[numberColumns[16]]
+            };
+            if (((Owner)owner).DRFO > 0)
+            { ((Owner)owner).Name = $"{((Owner)owner).F}\t{((Owner)owner).I}\t{((Owner)owner).O}"; }
+        }
+
+        public void SetOrderColumns(string nameColumnsInSource)
+        {
+            parsedColumns = nameColumnsInSource.Split('|');
+            nameColumns = new string[17];
+            numberColumns = new int[nameColumns.Length];
+
+            for (int i = 0; i < parsedColumns.Length; i++)
+            {
+                string s = parsedColumns[i]?.Trim();
+                switch (s)
+                {
+                    case "Назва власника":
+                        numberColumns[0] = i;
+                        break;
+                    case "Код ЄДРПОУ власника":
+                        numberColumns[3] = i;
+                        break;
+                    case "Код ДРФО власника":
+                        numberColumns[4] = i;
+                        break;
+                    case "Прізвище власника":
+                        numberColumns[5] = i;
+                        break;
+                    case "Ім'я власника":
+                        numberColumns[6] = i;
+                        break;
+                    case "По-батькові власника":
+                        numberColumns[7] = i;
+                        break;
+                    case "Дата народження власника":
+                        numberColumns[8] = i;
+                        break;
+                    case "Область/місто":
+                        numberColumns[9] = i;
+                        break;
+                    case "Н.П./район":
+                        numberColumns[10] = i;
+                        break;
+                    case "Назва вулиці":
+                        numberColumns[11] = i;
+                        break;
+                    case "Номер будинку":
+                        numberColumns[12] = i;
+                        break;
+                    case "Корпус":
+                        numberColumns[13] = i;
+                        break;
+                    case "Номер приміщення":
+                        numberColumns[14] = i;
+                        break;
+                    case "Код операції":
+                        numberColumns[15] = i;
+                        break;
+                    case "Дата операції":
+                        numberColumns[16] = i;
+                        break;
+                }
+            }
+        }
+
+        public IRow Get()
+        {
+            return owner;
+        }
+    }
+
+    public class ParserRowToCar
+    {
+        private IRow car;
+        private string _row;
+
+        public int[] numberColumns = new int[1];
+        public string[] parsedColumns = new string[1];
+        public string[] nameColumns = new string[1];
+
+
+        public ParserRowToCar(string rowData)
+        { _row = rowData; }
+
+        public void ConvertRowToModel()
+        {
+            parsedColumns = _row.Split('|');
+            car = new Car
+            {
+                Plate = parsedColumns[numberColumns[0]],
+                Factory = parsedColumns[numberColumns[1]],
+                Model = parsedColumns[numberColumns[2]],
+                ManufactureYear = parsedColumns[numberColumns[3]],
+                BodyNumber = parsedColumns[numberColumns[4]],
+                ChassisNumber = parsedColumns[numberColumns[5]],
+                EngineVolume = parsedColumns[numberColumns[6]]
+            };
             car.Name = car.ToString();
         }
 
         public void SetOrderColumns(string nameColumnsInSource)
         {
-            _orderNameColumns = new List<string>();
-            _orderNameColumns.Add(nameof(car.Plate)); //Державний номер
-            _orderNameColumns.Add(nameof(car.Factory));//Марка
-            _orderNameColumns.Add(nameof(car.Model));//Модель
-            _orderNameColumns.Add(nameof(car.ManufactureYear));//Рік випуска
-            _orderNameColumns.Add(nameof(car.BodyNumber));//№ кузова
-            _orderNameColumns.Add(nameof(car.ChassisNumber));//№ шасі 
-            _orderNameColumns.Add(nameof(car.EngineVolume));//Об'єм двигуна
+            Car name;
+            string[] _orderNameColumns = new string[]
+            {
+                nameof(name.Plate), //Державний номер
+                nameof(name.Factory),//Марка
+                nameof(name.Model),//Модель
+                nameof(name.ManufactureYear),//Рік випуска
+                nameof(name.BodyNumber),//№ кузова
+                nameof(name.ChassisNumber),//№ шасі 
+                nameof(name.EngineVolume)//Об'єм двигуна
+            };
 
             parsedColumns = nameColumnsInSource.Split('|');
-            nameColumns = new string[_orderNameColumns.ToArray().Length];
+            nameColumns = new string[_orderNameColumns.Length];
             numberColumns = new int[nameColumns.Length];
 
             for (int i = 0; i < parsedColumns.Length; i++)
@@ -299,10 +414,9 @@ namespace AutoAnalyse
             }
         }
 
-        public Car Get()
+        public IRow Get()
         {
             return car;
         }
     }
-
 }
