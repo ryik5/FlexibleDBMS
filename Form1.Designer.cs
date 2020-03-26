ebc9d3e2a4d289521a53fb6b5173c8aaa266ccdd
ryik5 <ryik.yuri@gmail.com>
2020-03-26 18:29:56 +0200

    fix a few mistakes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 131);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(177, 169);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,11 +16,19 @@ namespace AutoAnalyse
         public Form1()
         {
             InitializeComponent();
+            button1.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ImportData();
         }
 
         public void ImportData()
         {
-
+            FileReader<Car> reader = new FileReader<Car>();
+            IDictionary<string, Car> dic = reader.GetContent("1Model.txt");
+            textBox1.AppendText(dic.Count.ToString());
         }
     }
 
@@ -52,8 +60,7 @@ namespace AutoAnalyse
     {
         private string _name;
         public int ID { get; set; }
-
-        public string Plate { get; set; }
+                public string Plate { get; set; }
         public string Factory { get; set; }
         public string Model { get; set; }
         public string ManufactureYear { get; set; }
@@ -91,7 +98,6 @@ namespace AutoAnalyse
         public string Name { get; set; }
         public TypeOwner Type { get; set; }
         public string Address { get; set; }
-        //  { get { return $"{District}/t{City}/t{Street}/t{Building}/t{BuildingBody}/t{Apartment}"; } }
 
         public int EDRPOU { get; set; }
         public int DRFO { get; set; }
@@ -107,8 +113,7 @@ namespace AutoAnalyse
         public string Apartment { get; set; }
         public string CodeOperation { get; set; }
         public string CodeDate { get; set; }
-
-
+        
         public override string ToString()
         {
             string result;
@@ -171,23 +176,33 @@ namespace AutoAnalyse
 
         public IDictionary<string, T> GetContent(string filePath, Encoding encoding)
         {
+                    MessageBox.Show($"GetContent {typeof(T)}");
+
             IDictionary<string, T> dic = new Dictionary<string, T>();
+            T model;
             bool exist = false;
 
             var lines = File.ReadLines(filePath, encoding);
+
             foreach (var line in lines)
             {
-                T model = GetModel<T>.ToModel(line);
-                if (model != null && model.Name.Length > 0)
+                if (line?.Length > 10)
                 {
-                    if (dic.Count > 0)
-                    { exist = dic.ContainsKey(model.Name); }
+                    model = GetModel<T>.ToModel(line);
 
-                    if (!exist)
-                    { dic.Add(model.Name, model); }
+                    if (model != null && model.Name.Length > 0)
+                    {
+                        if (dic.Count > 0)
+                        { exist = dic.ContainsKey(model.Name); }
+
+                        if (!exist)
+                        {
+                            dic.Add(model.Name, model);
+                        }
+                    }
                 }
             }
-
+            MessageBox.Show("FileReader\n" + lines.Count()+"\n" + dic.Count.ToString());
             return dic;
         }
 
@@ -202,21 +217,24 @@ namespace AutoAnalyse
 
         public static T ToModel(string source)
         {
-            string s = typeof(T).ToString();
-            IRow row;
-            switch (s)
+            string className = typeof(T).ToString().Replace(typeof(T).Namespace.ToString()+".",""); //Get name class without namespace
+            T row;
+            switch (className)
             {
                 case "Car":
                     ParserRowToCar model = new ParserRowToCar(source);
-                    row = model.Get();
-                    return (T)row;
-
+                    model.SetOrderColumns("|Системный номер|Державний номер|Марка|Модель|Рік випуска|№ кузова|№ шасі|Об'єм двигуна|Код ДРФО власника|Прізвище власника|Ім'я власника|По-батькові власника|Дата народження власника|Код ЄДРПОУ власника|Назва власника|Область/місто|Н.П./район|Назва вулиці|Номер будинку|Корпус|Номер приміщення|Код операції|Дата операції|"); //first row
+                    row = (T)model.ConvertRowToModel();
+                    break;
                 case "Owner":
                 default:
                     ParserRowToOwner model1 = new ParserRowToOwner(source);
-                    row = model1.Get();
-                    return (T)row;
+                    model1.SetOrderColumns("|Системный номер|Державний номер|Марка|Модель|Рік випуска|№ кузова|№ шасі|Об'єм двигуна|Код ДРФО власника|Прізвище власника|Ім'я власника|По-батькові власника|Дата народження власника|Код ЄДРПОУ власника|Назва власника|Область/місто|Н.П./район|Назва вулиці|Номер будинку|Корпус|Номер приміщення|Код операції|Дата операції|"); //first row
+                    row = (T)model1.ConvertRowToModel();
+                    break;
             }
+
+            return (T)row;
         }
     }
 
@@ -226,16 +244,13 @@ namespace AutoAnalyse
         private string rowSource;
        
         public int[] numberColumns = new int[1];
-        public string[] parsedColumns = new string[1];
-        public string[] nameColumns = new string[1];
-
 
         public ParserRowToOwner(string rowSource)
         { this.rowSource = rowSource; }
 
-        public void ConvertRowToModel()
+        public IRow ConvertRowToModel()
         {
-            parsedColumns = rowSource.Split('|');
+            string[] parsedColumns = rowSource.Split('|');
             model = new Owner
             {
                 Name = parsedColumns[numberColumns[0]],
@@ -262,13 +277,14 @@ namespace AutoAnalyse
             };
             if (((Owner)model).DRFO > 0)
             { ((Owner)model).Name = $"{((Owner)model).F}\t{((Owner)model).I}\t{((Owner)model).O}"; }
+
+            return model;
         }
 
         public void SetOrderColumns(string nameColumnsInSource)
         {
-            parsedColumns = nameColumnsInSource.Split('|');
-            nameColumns = new string[17];
-            numberColumns = new int[nameColumns.Length];
+            string[] parsedColumns = nameColumnsInSource.Split('|');
+            numberColumns = new int[17];
 
             for (int i = 0; i < parsedColumns.Length; i++)
             {
@@ -324,28 +340,20 @@ namespace AutoAnalyse
             }
         }
 
-        public IRow Get()
-        {
-            return model;
-        }
     }
 
     public class ParserRowToCar
     {
         private IRow model;
         private string rowSource;
-
         public int[] numberColumns = new int[1];
-        public string[] parsedColumns = new string[1];
-        public string[] nameColumns = new string[1];
-
 
         public ParserRowToCar(string rowSource)
         { this.rowSource = rowSource; }
 
-        public void ConvertRowToModel()
+        public IRow ConvertRowToModel()
         {
-            parsedColumns = rowSource.Split('|');
+            string[] parsedColumns = rowSource.Split('|');
             model = new Car
             {
                 Plate = parsedColumns[numberColumns[0]],
@@ -357,25 +365,14 @@ namespace AutoAnalyse
                 EngineVolume = parsedColumns[numberColumns[6]]
             };
             model.Name = model.ToString();
+
+            return model;
         }
 
         public void SetOrderColumns(string nameColumnsInSource)
         {
-            Car name;
-            string[] _orderNameColumns = new string[]
-            {
-                nameof(name.Plate), //Державний номер
-                nameof(name.Factory),//Марка
-                nameof(name.Model),//Модель
-                nameof(name.ManufactureYear),//Рік випуска
-                nameof(name.BodyNumber),//№ кузова
-                nameof(name.ChassisNumber),//№ шасі 
-                nameof(name.EngineVolume)//Об'єм двигуна
-            };
-
-            parsedColumns = nameColumnsInSource.Split('|');
-            nameColumns = new string[_orderNameColumns.Length];
-            numberColumns = new int[nameColumns.Length];
+            string[] parsedColumns = nameColumnsInSource.Split('|');
+            numberColumns = new int[7];
 
             for (int i = 0; i < parsedColumns.Length; i++)
             {
@@ -383,40 +380,29 @@ namespace AutoAnalyse
                 switch (s)
                 {
                     case "Державний номер":
-                        nameColumns[0] = _orderNameColumns[0];
                         numberColumns[0] = i;
                         break;
                     case "Марка":
-                        nameColumns[1] = _orderNameColumns[1];
                         numberColumns[1] = i;
                         break;
                     case "Модель":
-                        nameColumns[2] = _orderNameColumns[2];
                         numberColumns[2] = i;
                         break;
                     case "Рік випуска":
-                        nameColumns[3] = _orderNameColumns[3];
                         numberColumns[3] = i;
                         break;
                     case "№ кузова":
-                        nameColumns[4] = _orderNameColumns[4];
                         numberColumns[4] = i;
                         break;
                     case "№ шасі":
-                        nameColumns[5] = _orderNameColumns[5];
                         numberColumns[5] = i;
                         break;
                     case "Об'єм двигуна":
-                        nameColumns[6] = _orderNameColumns[6];
                         numberColumns[6] = i;
                         break;
                 }
             }
         }
 
-        public IRow Get()
-        {
-            return model;
-        }
     }
 }
